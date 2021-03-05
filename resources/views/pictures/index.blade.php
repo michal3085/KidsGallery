bf495014a01090eb8acb25e899f70a03fb8e74fb
michal3085 <michal3085@gmail.com>
2021-03-05 18:09:44 +0100

    firts type of container in pictures index

--- resources/views/pictures/index.blade.php
+++ resources/views/pictures/index.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     @foreach($pictures as $picture)
-        <img src="{{ asset('/storage') . '/' . $picture->file_path }}" class="img-thumbnail">
+        <div class="container">
+            <div class="row section-box">
+                <div class="col-sm-4 text-center description-text">
+                    <img src="{{ asset('/storage') . '/' . $picture->file_path }}" class="img-thumbnail">
+                    <b>{{ $picture->user }}</b> | {{ $picture->name }}
+                </div>
+            </div>
+        </div>
+
+
     @endforeach
 @endsection
