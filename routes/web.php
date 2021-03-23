aaf59730be23c81f1f14df0cc8a2218a43f04c0a
michal3085 <michal3085@gmail.com>
2021-03-24 00:09:53 +0100

    SMAL-26 new like system complete

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,6 @@ Route::get('/', [PicturesController::class, 'index']);
 
 // Routes for pictures
 Route::get('/pictures/top10', [PicturesController::class, 'top'])->name('top10');
-//Route::post('/pictures/newlike/{id}', [PicturesController::class, 'like'])->name('pictures.newlike')->middleware('auth');
 Route::get('/pictures/{id}/report', [PicturesController::class, 'report'])->name('pictures.report');
 Route::put('/pictures/{id}/send/report', [PicturesController::class, 'SendReport'])->name('pictures.sendreport');
 Route::resource('pictures', PicturesController::class);
