83ab932e4affb39a0504c1d6c97eca225410dd4f
michal3085 <michal3085@gmail.com>
2021-03-23 22:05:26 +0100

    SMAL-2 new like system - unloged view likes count fixed

--- resources/views/unloged/show.blade.php
+++ resources/views/unloged/show.blade.php
@@ -25,7 +25,7 @@
                     </div>
                     <form action="{{ route('pictures.newlike', ['id' => $pictures->id]) }}" method="post">
                         @csrf
-                        <button type="submit" class="btn btn-success px-3"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $pictures->likes }}</button>
+                        <button type="submit" class="btn btn-success px-3"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $pictures->likes()->where('picture_id', $pictures->id)->count() }}</button>
                     </form>
                     <br>
                         <a href="{{ route('pictures.report', ['id' => $pictures->id]) }}">
