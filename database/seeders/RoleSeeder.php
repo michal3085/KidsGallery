00c7b7b61404ac2b29c15f9e83d249bb56647290
Michał Broszkiewicz <michal3085@gmail.com>
2021-05-02 15:16:21 +0200

    SMAL-63 seeders for users and roles fix2

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,9 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use App\Models\User;
-use App\Models\Role;
-use Illuminate\Support\Facades\Hash;
 
 class RoleSeeder extends Seeder
 {
@@ -17,25 +14,6 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                "id" => 1,
-                "name" => env('ADMIN_NAME'),
-                "email" => env('ADMIN_EMAIL'),
-                "password" => Hash::make(env('ADMIN_PASSWORD')),
-                "active" => 1,
-                "avatar" => "avatar/avatar1.png",
-            ],
-            [
-                "id" => 2,
-                "name" => env('MODERATOR_NAME'),
-                "email" => env('MODERATOR_EMAIL'),
-                "password" => Hash::make(env('MODERATOR_PASSWORD')),
-                "active" => 1,
-                "avatar" => "avatar/avatar1.png",
-            ]
-        ]);
-
         DB::table('roles')->insert([
             [
                 "user_id" => 1,
