cd23f852f18e569de373efef1286286b6f52ff27
Michał Broszkiewicz <michal3085@gmail.com>
2021-05-13 14:24:18 +0200

    SMAL-72 new gallery system in my gallery view

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,9 +37,26 @@ class UsersController extends Controller
     {
         $user = User::where('id', Auth::id())->first();
         $usersIds = $user->following()->pluck('follow_id')->all();
-        //$usersIds[] = $user->id;
-        $pictures = Picture::whereIn('user_id', $usersIds)->where('accept', 1)->latest()->paginate(10);
+        $rights = $user->followers()->where('follow_id', $user->id)->where('rights', 1)->pluck('user_id')->all();
 
+       // collecting the IDs of users I am following
+       $pics_set1 = Picture::whereIn('user_id', $usersIds)->where('visible', 1)->pluck('id');
+
+       // collecting id's of users who let me see their hidden pictures
+       $pics_set2 = Picture::where('visible', 0)->whereIn('user_id', $rights)->pluck('id');
+
+       /*
+        * Merging ID's
+        */
+       $set1_count = count($pics_set1);
+       $set2_count = count($pics_set2);
+
+       for ($i=0; $i<=$set2_count-1; $i++) {
+            $pics_set1[$set1_count] = $pics_set2[$i];
+            $set1_count++;
+       }
+       // downloading images from id's acquired earlier
+       $pictures = Picture::whereIn('id', $pics_set1)->where('accept', 1)->latest()->paginate(8);
 
         return view('users.index', compact('pictures'));
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -13,78 +13,65 @@
             <strong>{{ session()->get('message2') }}</strong>
         </div>
     @endif
-
-    <section class="resume-section" id="about">
-        <div class="resume-section-content">
-            <h1 class="mb-0">
-                {{ \Illuminate\Support\Facades\Auth::user()->name }}
-            </h1>
+    <div class="container-fluid">
+        <div class="px-lg-5">
+            <!-- For demo purpose -->
+            <div class="row py-5">
+                <div class="col-lg-12 mx-auto">
+                    <div class="text-white p-5 shadow-sm rounded banner">
+                        <h1 class="display-4">{{ __('Yours Gallery') }}</h1>
+                    </div>
+                </div>
+            </div>
+            <!-- End -->
+            <div class="row">
             @foreach($pictures as $picture)
-                @if ( $picture->visible == 0)
-                    @if( \App\Models\Follower::where('user_id', $picture->user_id)->where('follow_id', $user->id)->where('rights', 1)->count() != 0 )
-                        <p class="lead mb-5">
-                        <div class="row section-box">
-                            <div class="col-sm-xl text-center description-text shadow p-3 mb-5 rounded">
-                                <a href="{{ route('pictures.show', ['picture' => $picture->id]) }}">
-                                    <img src="{{ asset('/storage') . '/' . $picture->file_path }}" class="img-thumbnail">
-                                </a>
-                                <br>
-                                <b>{{ $picture->user }}</b> | {{ $picture->name }}
-                                <br>
-                                <i class="fas fa-calendar-week"></i>: {{ $picture->created_at }}
-                                | <i class="far fa-eye"></i> {{ $picture->views }}
+                <!-- Gallery item -->
+                    <div class="col-xl-3 col-lg-4 col-md-6 mb-4">
+                        <div class="bg-white rounded shadow p-1 mb-5 rounded">
+                            <a href="{{ route('pictures.show', ['picture' => $picture->id]) }}">
+                                <img src="{{ asset('/storage') . '/' . $picture->file_path }}" alt="" class="img-fluid card-img-top">
+                            </a>
+                            <div class="p-4">
+                                <h5> <a href="{{ route('pictures.show', ['picture' => $picture->id]) }}" class="text-dark">{{ $picture->name }}</a></h5>
+                                <p class="small text-muted mb-0">
+                                    <a href="{{ route('profiles.about', ['name' => $picture->user ]) }}"><img class="img-fluid img-responsive rounded-circle mr-2" src="{{ asset('/storage') . '/' . \App\Models\User::where(['name' => $picture->user])->pluck('avatar')->first() }}" alt="user" style="width: 25px; height: 25px;">
+                                        {{ $picture->user }}</a>
+                                </p>
+                                <hr>
+                                @if ($picture->likes()->where('picture_id', $picture->id)->where('user_id', \Illuminate\Support\Facades\Auth::id())->count() == 0)
+                                    <button type="submit" class="btn btn-outline-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
+                                @else
+                                    <button type="submit" class="btn btn-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
+                                @endif
                             </div>
                         </div>
-                        @if ($picture->likes()->where('picture_id', $picture->id)->where('user_id', \Illuminate\Support\Facades\Auth::id())->count() == 0)
-                            <button type="submit" class="btn btn-outline-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
-                        @else
-                            <button type="submit" class="btn btn-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
-                        @endif
-                    @endif
-                @endif
-                @if ($picture->visible == 1)
-                    <p class="lead mb-5">
-                    <div class="row section-box">
-                        <div class="col-sm-xl text-center description-text shadow p-3 mb-5 rounded">
-                            <a href="{{ route('pictures.show', ['picture' => $picture->id]) }}">
-                                <img src="{{ asset('/storage') . '/' . $picture->file_path }}" class="img-thumbnail">
-                            </a>
-                            <br>
-                            <b>{{ $picture->user }}</b> | {{ $picture->name }}
-                            <br>
-                            <i class="fas fa-calendar-week"></i>: {{ $picture->created_at }}
-                            | <i class="far fa-eye"></i> {{ $picture->views }}
-                        </div>
                     </div>
-                    @if ($picture->likes()->where('picture_id', $picture->id)->where('user_id', \Illuminate\Support\Facades\Auth::id())->count() == 0)
-                        <button type="submit" class="btn btn-outline-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
-                    @else
-                        <button type="submit" class="btn btn-success px-3 like" data-id="{{ $picture->id }}"><i class="far fa-thumbs-up" aria-hidden="true"></i>  {{ $picture->likes()->where('picture_id', $picture->id)->count() }}</button>
-                    @endif
-                @endif
-            @endforeach
-            {{--            <div class="pagination pagination-lg justify-content-center">--}}
+                    <!-- End -->
+                @endforeach
+
+            </div>
             <div class="pagination justify-content-center">
                 {{ $pictures->links() }}
             </div>
         </div>
-    </section>
+    </div>
 @endsection
 @section('javascript')
     $(function() {
-            $('.like').click( function () {
-            $.ajax({
-                method: "POST",
-                url: "/newlike/" + $(this).data("id")
-                // data: { name: "John", location: "Boston" }
-            })
-                .done(function( response ) {
-                window.location.reload();
-            })
-                // .fail(function( response ) {
-                // alert( "Juz polubione" );
-                // });
-            });
+    $('.like').click( function () {
+    $.ajax({
+    method: "POST",
+    url: "/newlike/" + $(this).data("id")
+    // data: { name: "John", location: "Boston" }
+    })
+    .done(function( response ) {
+    window.location.reload();
+    })
+    .fail(function( response ) {
+    alert( "Juz polubione" );
+    });
+    });
     });
 
 @endsection
