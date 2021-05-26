87094ae27d1586500eed4234e1f6ae290d8bf790
michal3085 <michal3085@gmail.com>
2021-05-26 15:11:20 +0200

    SMAL-61 green background color on new messages

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -19,8 +19,12 @@
                         <button class="btn btn-outline-primary" type="submit"><i class="fas fa-search"></i></button></div>
                 </form>
                 @foreach($conversations as $conversation)
-                    <div class="d-flex flex-row comment-row">
-                        @if ($conversation->user_a == \Illuminate\Support\Facades\Auth::user()->name)
+                    @if (\App\Models\Message::where('conversation_id', $conversation->id)->where('to_id', auth()->id())->where('read', 0)->count())
+                        <div class="d-flex flex-row comment-row" style="background-color: rgba(66,232,11,0.13); border-radius: 25px;">
+                        @else
+                                <div class="d-flex flex-row comment-row">
+                                    @endif
+                                @if ($conversation->user_a == \Illuminate\Support\Facades\Auth::user()->name)
                                 <div class="p-2"><span class="round"><img class="img-fluid img-responsive rounded-circle mr-2 shadow rounded" src="{{ asset('/storage') . '/' . \App\Models\User::where(['name' => $conversation->user_b])->pluck('avatar')->first() }}" alt="user" style="width: 50px; height: 50px;"></span></div>
                                 <div class="comment-text w-100">
                                 <a href="{{ route('messages.show', ['id' => $conversation->id, 'to' => $conversation->user_b]) }}"><h5>{{ $conversation->user_b }}</h5></a>
@@ -34,7 +38,7 @@
                             <p class="m-b-5 m-t-10"></p>
                         </div>
                     </div>
-                    <hr>
+                                    <hr>
                 @endforeach
             </div>
         </section>
