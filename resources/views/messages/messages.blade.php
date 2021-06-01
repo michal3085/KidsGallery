574fbbd6f4a46789beec50f70f96ae0fa30130a8
michal3085 <michal3085@gmail.com>
2021-06-01 21:20:03 +0200

    SMAL-61 text alagin left for both message type date and name aligin fix

--- resources/views/messages/messages.blade.php
+++ resources/views/messages/messages.blade.php
@@ -37,18 +37,18 @@
                                             </a>
                                             <div class="status offline"></div>
                                         </div>
-                                        <a href="{{ route('profiles.about', ['name' => $message->from ]) }}"><div class="name">{{ $message->from }}</div></a>
+                                        <a href="{{ route('profiles.about', ['name' => $message->from ]) }}"><div class="namer">{{ $message->from }}</div></a>
                                         <div class="text">
                                             {{ $message->message }}
                                         </div>
                                         @if ($message->deleted == 0)
-                                        <div class="name"><i class="far fa-trash-alt delete" data-id="{{ $message->id }}"></i> {{ $message->created_at }}
+                                        <div class="namer"><i class="far fa-trash-alt delete" data-id="{{ $message->id }}"></i> {{ $message->created_at }}
                                             @if ($message->read == 1)
                                                 <i class="far fa-eye" data-toggle="tooltip" data-html="true" data-title="Wiadomość wyświetlona przez <b>{{ $message->to }}</b>: <br> {{ $message->updated_at }}"  data-delay='{"show":"500", "hide":"300"}'></i>
                                             @endif
                                         </div>
                                         @else
-                                               <div class="name">{{ $message->created_at }}
+                                               <div class="namer">{{ $message->created_at }}
                                                    <i class="far fa-eye" data-toggle="tooltip" data-html="true" data-title="Wiadomość wyświetlona przez <b>{{ $message->to }}</b>: <br> {{ $message->updated_at }}"  data-delay='{"show":"500", "hide":"300"}'></i>
                                                </div>
                                         @endif
@@ -65,14 +65,14 @@
                                             </a>
                                           <div class="status offline"></div>
                                         </div>
-                                        <a href="{{ route('profiles.about', ['name' => $message->from ]) }}"><div class="name">{{ $message->from }}</div></a>
+                                        <a href="{{ route('profiles.about', ['name' => $message->from ]) }}"><div class="namel">{{ $message->from }}</div></a>
                                         <div class="text">
                                             {{ $message->message }}
                                         </div>
                                            @if ($message->deleted == 0)
-                                                <div class="name">{{ $message->created_at }} <i class="fas fa-exclamation report" data-id="{{ $message->id }}"></i></div>
+                                                <div class="namel">{{ $message->created_at }} <i class="fas fa-exclamation report" data-id="{{ $message->id }}"></i></div>
                                            @else
-                                               <div class="name">{{ $message->created_at }}</div>
+                                               <div class="namel">{{ $message->created_at }}</div>
                                            @endif
                                     </div>
                                 @endif
