f1a2eea4e30ea71ec9b41a5b8445c1a114de00ce
michal3085 <michal3085@gmail.com>
2021-06-10 19:41:51 +0200

    Added user avatar in mobile view

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -20,9 +20,14 @@
 </head>
 <body id="page-top">
 <!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow-lg p-3 mb-5 rounded" id="sideNav">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow-lg p-2 mb-5 rounded" id="sideNav">
     <a href="{{ route('profiles.gallery', ['name' => $user->name]) }}" class="navbar-brand js-scroll-trigger">
-        <span class="d-block d-lg-none">SmallGallery</span>
+        <span class="d-block d-lg-none">
+            <div class="avatar">
+                  <img class="img-fluid img-profile rounded-circle mx-auto shadow p-1 rounded" src="{{ asset('/storage') . '/' . $user->avatar }}" alt="" style="width: 50px; height: 50px;" />
+            <div class="status offline"></div>
+            </div>
+        </span>
         <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2 shadow p-2 mb-3 rounded" src="{{ asset('/storage') . '/' . $user->avatar }}" alt="" style="width: 160px; height: 160px;" /></span>
      </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
